4f0b10803a1d9bc5080d9159944ffee899625972
G02\HONGUF <flaviuemanuel.hongu@fujitsu.com>
2023-02-20 12:32:30 +0100

    Implemented loadUsers() function to fetch data from the server.

--- src/main/resources/static/js/users-datatables.js
+++ src/main/resources/static/js/users-datatables.js
@@ -5,4 +5,15 @@ $(document).ready(function() {
 });
 
 function loadUsers() {
+  const rawResponse = await fetch('', {
+    method: 'POST',
+    headers: {
+      'ACCEPT': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({a: 1, b: 'Textual content'})
+  });
+  const content = await rawResponse.json();
+
+  console.log(content);
 }
